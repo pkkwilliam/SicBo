fb1ce43dbc78e556e6dc6b4f36cc13f8abcf49e9
Ka Kei Pun <pkkwilliam@Kas-MacBook-Pro.local>
2019-12-14 12:09:38 +0800

    Updates

---
+++ precache-manifest.b7001a883642c1455d59fe30450ee032.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a1ecc9736ebe75e1263edc39109b1b",
+    "url": "/pkkwilliam/SicBo/index.html"
+  },
+  {
+    "revision": "540a8fa745cc81d042bd",
+    "url": "/pkkwilliam/SicBo/static/css/main.a0da3455.chunk.css"
+  },
+  {
+    "revision": "8a1b18428c5865a716d8",
+    "url": "/pkkwilliam/SicBo/static/js/2.c76883cd.chunk.js"
+  },
+  {
+    "revision": "540a8fa745cc81d042bd",
+    "url": "/pkkwilliam/SicBo/static/js/main.aa07cfff.chunk.js"
+  },
+  {
+    "revision": "519422c27111f1ed94e9",
+    "url": "/pkkwilliam/SicBo/static/js/runtime-main.b7298929.js"
+  }
+]);
