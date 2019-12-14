1c233b40735bbd99bd45371991f6b1a1dda29774
Ka Kei Pun <pkkwilliam@Kas-MacBook-Pro.local>
2019-12-14 12:27:18 +0800

    Updates

---
+++ precache-manifest.f2a0d2634cdab17b5598561cadfaff69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4a20475bb4e10d4e36f2843b0f907f",
+    "url": "/SicBo/index.html"
+  },
+  {
+    "revision": "606dad9b983624607422",
+    "url": "/SicBo/static/css/main.a0da3455.chunk.css"
+  },
+  {
+    "revision": "8a1b18428c5865a716d8",
+    "url": "/SicBo/static/js/2.c76883cd.chunk.js"
+  },
+  {
+    "revision": "606dad9b983624607422",
+    "url": "/SicBo/static/js/main.9512d7a3.chunk.js"
+  },
+  {
+    "revision": "fa60047ba3f54e7fcc9b",
+    "url": "/SicBo/static/js/runtime-main.de88f234.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pkkwilliam/SicBo/precache-manifest.b7001a883642c1455d59fe30450ee032.js"
+  "/SicBo/precache-manifest.f2a0d2634cdab17b5598561cadfaff69.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pkkwilliam/SicBo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SicBo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
